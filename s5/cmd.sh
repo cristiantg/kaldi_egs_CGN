a69cdb0ccf5f486a2480b1972eafa1624979ce07
Arif Khan <ifeeforall@gmail.com>
2021-02-17 09:19:12 +0100

    Path and cmd configuration on ponyland

--- s5/cmd.sh
+++ s5/cmd.sh
@@ -3,28 +3,40 @@
 # change this to a queue you have access to.
 # Otherwise, use "run.pl", which will run jobs locally
 # (make sure your --num-jobs options are no more than
-# the number of cpus on your machine.
+# the number of cpus on your machine.i
 
-#a) JHU cluster options
-#export train_cmd="queue.pl -l arch=*64"
-#export decode_cmd="queue.pl -l arch=*64 --mem 2G"
-#export mkgraph_cmd="queue.pl -l arch=*64 --mem 4G"
-#export big_memory_cmd="queue.pl -l arch=*64 --mem 8G"
-#export cuda_cmd="queue.pl -l gpu=1"
+# hostname=$(hostname)
 
-#b) run it locally...
-export train_cmd=run.pl
-export decode_cmd=run.pl
-export cuda_cmd=run.pl
-export mkgraph_cmd=run.pl
+hostname=$(hostname)
 
-#c) BUT cluster:
-#if [ "$(hostname -d)" == "fit.vutbr.cz" ]; then
-#  queue="all.q@@blade,all.q@@speech"
-#  gpu_queue="long.q@supergpu*,long.q@dellgpu*,long.q@pcspeech-gpu,long.q@pcgpu*"
-#  storage="matylda5"
-#  export train_cmd="queue.pl -q $queue -l ram_free=1500M,mem_free=1500M,${storage}=1"
-#  export decode_cmd="queue.pl -q $queue -l ram_free=2500M,mem_free=2500M,${storage}=0.5"
-#  export cuda_cmd="queue.pl -q $gpu_queue -l gpu=1" 
-#fi
+if [ "${hostname:0:3}" == "mlp" ]; then 
+echo "running on ponyland mahcines"
+echo "running on $hostname"
+export train_cmd="slurm_pony.pl -p short --time 11:59:00 --mem 4G -x mlp03"
+export ivector_cmd="slurm_pony.pl -p short --time 11:59:00 --mem 4G -x mlp03"
+export align_cmd="slurm_pony.pl -p short --time 11:59:00 --mem 4G -x mlp03"
+export decode_cmd="slurm_pony.pl -p short --time 11:59:00 --mem 10G -x mlp03"
+export mkgraph_cmd="slurm_pony.pl -p short --time 11:59:00 --mem 8G -x mlp03"
+export rnn_cmd="slurm_pony.pl -p short --num-threads 1 --gpu 1 --time 08:59:00 --mem 12G"
+export cuda_cmd="slurm_pony.pl -p short --num-threads 1 --time 00:10:00 --mem 4G"
+
+elif [ "${hostname:0:4}" == "coma" ]; then
+export train_cmd="slurm_coma.pl -p normal --time 11:59:00 --mem 10G"
+export decode_cmd="slurm_coma.pl -p normal --time 00:30:00 --mem 10G"
+export mkgraph_cmd="slurm_coma.pl -p normal --time 01:59:00 --mem 10G"
+export cuda_cmd="slurm_coma.pl -p normal --time 00:30:00 --mem 6G"
+export cuda_cmd="slurm.pl -p long -w "coma13" --time 23:59:00 --mem 6G"
+
+else 
+echo "No squeue or qstat found, running local"
+
+export train_cmd=utils/run.pl
+export decode_cmd=utils/run.pl
+export cuda_cmd=utils/run.pl
+export mkgraph_cmd=utils/run.pl
+export align_cmd=utils/run.pl
+export ivector_cmd=utils/run.pl
+
+fi
+
 
