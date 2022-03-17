d3ed6952a3272ed8b2a03710534679a9b572b88b
Cristian TG <cristian.tejedor.91@gmail.com>
2022-03-17 16:31:36 +0100

    Example of how to decode a single file in real-time

---
+++ s5/server-load.sh
@@ -0,0 +1,1 @@
+./uber_single.sh /vol/tensusers4/ctejedor/lanewcristianmachine/opt/kaldi/egs/kaldi_egs_CGN/s5/_runs2022/exp/tdnn1a_sp_bi_online raw_data/henk henk.wav raw_data/henk henk2 speaker > logfile 2>&1
